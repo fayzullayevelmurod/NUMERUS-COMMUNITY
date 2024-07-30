fcbd5dd184da2c9622483dd017d523326012221b
SardorQobilov <sardorqobilov555@gmail.com>
2024-07-30 19:07:13 +0500

    home page media 100% finished

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const selectedLang = document.querySelector(".selected-lang");
+  const optionLanguage = document.querySelector(".option-language");
+
+  selectedLang.addEventListener("click", function () {
+    optionLanguage.classList.toggle("show");
+    selectedLang.classList.toggle("active");
+  });
+
+  optionLanguage.addEventListener("click", function () {
+    // selected-lang va option-language elementlaridagi flag va name elementlarini olish
+    const selectedFlag = selectedLang.querySelector(".lang-flag");
+    const selectedName = selectedLang.querySelector(".lang-name");
+    const optionFlag = optionLanguage.querySelector(".lang-flag");
+    const optionName = optionLanguage.querySelector(".lang-name");
+
+    // flag va name elementlarini almashtirish
+    [selectedFlag.textContent, optionFlag.textContent] = [
+      optionFlag.textContent,
+      selectedFlag.textContent,
+    ];
+    [selectedName.textContent, optionName.textContent] = [
+      optionName.textContent,
+      selectedName.textContent,
+    ];
+
+    // show va active classlarini olib tashlash
+    optionLanguage.classList.remove("show");
+    selectedLang.classList.remove("active");
+  });
+  window.addEventListener("click", (e) => {
+    if (!selectedLang.contains(e.target) && !optionLanguage.contains(e.target)) {
+      optionLanguage.classList.remove("show");
+      selectedLang.classList.remove("active");
+    }
+  });
+});
