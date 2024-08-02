9ef1b840b84ce9d1e02a722ffb1b3157afc76b5c
SardorQobilov <sardorqobilov555@gmail.com>
2024-08-03 00:16:12 +0500

    personal-account-two.html page finished

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const selectedLang = document.querySelector(".selected-lang");
-  const optionLanguage = document.querySelector(".option-language");
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+    const selectedLang = document.querySelector('.selected-lang');
+    const optionLanguage = document.querySelector('.option-language');
 
-  selectedLang.addEventListener("click", function () {
-    optionLanguage.classList.toggle("show");
-    selectedLang.classList.toggle("active");
-  });
+    selectedLang.addEventListener('click', function () {
+      optionLanguage.classList.toggle('show');
+      selectedLang.classList.toggle('active');
+    });
 
-  optionLanguage.addEventListener("click", function () {
-    // selected-lang va option-language elementlaridagi flag va name elementlarini olish
-    const selectedFlag = selectedLang.querySelector(".lang-flag");
-    const selectedName = selectedLang.querySelector(".lang-name");
-    const optionFlag = optionLanguage.querySelector(".lang-flag");
-    const optionName = optionLanguage.querySelector(".lang-name");
-    console.log(selectedFlag.src);
+    optionLanguage.addEventListener('click', function () {
+      // selected-lang va option-language elementlaridagi flag va name elementlarini olish
+      const selectedFlag = selectedLang.querySelector('.lang-flag');
+      const selectedName = selectedLang.querySelector('.lang-name');
+      const optionFlag = optionLanguage.querySelector('.lang-flag');
+      const optionName = optionLanguage.querySelector('.lang-name');
+      console.log(selectedFlag.src);
 
-    // flag va name elementlarini almashtirish
-    [selectedFlag.src, optionFlag.src] = [
-      optionFlag.src,
-      selectedFlag.src,
-    ];
-    [selectedName.textContent, optionName.textContent] = [
-      optionName.textContent,
-      selectedName.textContent,
-    ];
+      // flag va name elementlarini almashtirish
+      [selectedFlag.src, optionFlag.src] = [optionFlag.src, selectedFlag.src];
+      [selectedName.textContent, optionName.textContent] = [
+        optionName.textContent,
+        selectedName.textContent,
+      ];
 
-    // show va active classlarini olib tashlash
-    optionLanguage.classList.remove("show");
-    selectedLang.classList.remove("active");
-  });
-  window.addEventListener("click", (e) => {
-    if (!selectedLang.contains(e.target) && !optionLanguage.contains(e.target)) {
-      optionLanguage.classList.remove("show");
-      selectedLang.classList.remove("active");
-    }
-  });
+      // show va active classlarini olib tashlash
+      optionLanguage.classList.remove('show');
+      selectedLang.classList.remove('active');
+    });
+    window.addEventListener('click', (e) => {
+      if (
+        !selectedLang.contains(e.target) &&
+        !optionLanguage.contains(e.target)
+      ) {
+        optionLanguage.classList.remove('show');
+        selectedLang.classList.remove('active');
+      }
+    });
+  } catch (error) {}
 });
